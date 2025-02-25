65c4a2aa2d71f80f440f4ecda51ec38097df8d48
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2025-02-24 22:23:17 -0600

    Create readme.rs

---
+++ Research:/Ideal Metals/readme.rs
@@ -0,0 +1,12 @@
+/* We want a metal that is Cheap, having Low Reactivity, easy to recover & purify again, and has a Melting point close to the Pyrolysis temperature of the Feedstock!
+*/
+
+let knowledge = ['https://www.metalsupermarkets.com/melting-points-of-metals/'];
+
+let metals_database = ['https://fractory.com/melting-point-of-metals-chart/', ''];
+
+let search = ['https://www.google.com/search?q=melting+point+of+metals', 'https://www.google.com/search?q=metals+with+lowest+melting+points', 'https://www.google.com/search?q=metals+by+melting+point'];
+
+let options = [Zinc, Tin, Lead];
+
+let alterantive = [MoltenSalt];
