373e815dd93d66f07b19d7b85a83dd1d306f12cc
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2025-02-24 22:27:48 -0600

    Update and rename readme.md to readme.rs

--- Research:/Molten Salt/readme.md
+++
@@ -1,3 +0,0 @@
-search = ['https://www.google.com/search?q=molten+salt','https://www.google.com/search?q=molten+salt+melting+point','https://www.google.com/search?q=molten+salt+reactor'];
-
-wiki = ['https://en.wikipedia.org/wiki/Molten_salt','https://en.wikipedia.org/wiki/Molten-salt_reactor'];
---
+++ Research:/Molten Salt/readme.rs
@@ -0,0 +1,5 @@
+let search = ['https://www.google.com/search?q=molten+salt','https://www.google.com/search?q=molten+salt+melting+point','https://www.google.com/search?q=molten+salt+reactor'];
+
+let wiki = ['https://en.wikipedia.org/wiki/Molten_salt','https://en.wikipedia.org/wiki/Molten-salt_reactor'];
+
+let wiki_relation = ['https://en.wikipedia.org/wiki/Heat-transfer_fluid','https://en.wikipedia.org/wiki/Eutectic_system'];
